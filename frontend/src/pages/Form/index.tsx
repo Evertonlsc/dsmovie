db5ab07eb118418ac5720b7a313bd3649643581c
Evertonlsc <evertonluizcruzs@gmail.com>
2022-05-14 05:10:58 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Form() {
@@ -28,13 +29,17 @@ function Form() {
                             <option>3</option>
                             <option>4</option>
                             <option>5</option>
+                            <option>6</option>
                         </select>
                     </div>
                     <div className="dsmovie-form-btn-container">
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+                
             </div >
         </div >
     );
